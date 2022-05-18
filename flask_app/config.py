29856f57d32f453fb1c9e45751589069d29b8f50
Mateusz Małecki <244954@student.pwr.edu.pl>
2022-05-18 20:57:33 +0200

    Prod fix

--- flask_app/config.py
+++ flask_app/config.py
@@ -1,9 +1,9 @@
 SECRET_KEY = 'secret!'
-env = 'dev'
+env = 'prod'
 SQLALCHEMY_DATABASE_URI_DEV = 'postgresql://postgres:Spanko123@localhost/cinema-booking-service'
 SQLALCHEMY_DATABASE_URI_PROD = 'postgres://zvbrepbrzinmob' \
                                ':353756c35468cb6c43a142b686d2f9120dfcc88968d95aeeb7a0e33fbcb5c542@ec2-52-18-116-67.eu' \
-                               '-west-1.compute.amazonaws.com:5432/dddakce3tomshd '
+                               '-west-1.compute.amazonaws.com:5432/dddakce3tomshd'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 CORS_HEADERS = 'Content-Type'
 DB_TYPE = 'SQLAlchemy'
